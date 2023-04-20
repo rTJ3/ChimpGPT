b1d99aa674b21038dfaeedfae7c22a8eac7ff857
rTJ3 <100946184+rTJ3@users.noreply.github.com>
2023-04-20 18:11:30 +0900

    Updated
    Fixed file structure for ghp

--- server.js
+++ server.js
@@ -6,36 +6,11 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Routes
-app.get('/leaderboard', (req, res) => {
-  fs.readFile('leaderboard.json', 'utf8', (err, data) => {
-    if (err) throw err;
-    res.send(JSON.parse(data));
-  });
-});
-
-app.post('/leaderboard', (req, res) => {
-  const newEntry = req.body;
-  fs.readFile('leaderboard.json', 'utf8', (err, data) => {
-    if (err) throw err;
-    const leaderboard = JSON.parse(data);
-    leaderboard.push(newEntry);
-    leaderboard.sort((a, b) => b.score - a.score);
-    if (leaderboard.length > 10) {
-      leaderboard.pop();
-    }
-    fs.writeFile('leaderboard.json', JSON.stringify(leaderboard), (err) => {
-      if (err) throw err;
-      res.send('Leaderboard updated');
-    });
-  });
-});
+app.use(express.static(path.join(__dirname,)));
 
 // Add the code snippet here, just before the app.listen() call
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Start server
